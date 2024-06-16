d5bedcf642eaa547c71bf22516a72d1c1907f86d
itzrahul2002 <119419147+itzrahul2002@users.noreply.github.com>
2024-06-16 21:56:17 +0530

    Add files via upload

---
+++ DSA/src/All_Prime.java
@@ -0,0 +1,15 @@
+public class All_Prime {
+    static void Prime(int n){
+        for(int i=2; i<=n/2; i++){
+            if(n%i==0) {
+                System.out.println(i + " NOT");
+            }
+            else {
+                System.out.println(i+ "Prime");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Prime(20);
+    }
+}
